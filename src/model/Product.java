b9696965e1d75300b255d0da93487c73d973b1aa
JhomStudy <Jhonatam.santos096@academico.ifs.edu.br>
2020-10-01 09:08:46 -0300

    create a crud project

---
+++ src/model/Product.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Product {
+  private String name;
+  private double price;
+  private int quantity;
+
+  public Product() {
+  }
+
+  public Product(String name, double price, int quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  
+
+}
